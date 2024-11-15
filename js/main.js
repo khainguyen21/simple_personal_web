5498303290f76d6c5c8a9e20f84e13e62c9717c4
Khai Nguyen <khainguyen@Khais-MacBook-Pro.local>
2024-11-14 21:36:48 -0800

    Version 1

---
+++ js/main.js
@@ -0,0 +1,52 @@
+//Menu
+const menu = document.querySelector(".navbar__links");
+const menuButton = document.querySelector(".navbar__icons");
+const overlay = document.querySelector("#overlay");
+
+menuButton.addEventListener('click', ()=>{
+    menu.classList.toggle("navbar__open"); 
+    menuButton.classList.toggle("open"); 
+    overlay.classList.toggle("show");
+
+}); 
+
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove("navbar__open"); 
+    menuButton.classList.remove("open"); 
+    overlay.classList.toggle("show");
+
+});
+
+gsap.from('header', {duration: 1 , opacity: 0, delay: 2, stagger: .5})
+gsap.from('section', {duration: 1 , opacity: 0, x: '-100%', delay: 3, stagger: .5}); 
+
+
+//Flip Card
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('click', function(){
+        card.classList.toggle('card__flip');
+    });
+});
+
+$(document).ready(function(){
+    $('a')  .on('click', function(e){
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 1000);
+
+    });
+    
+});
+
+
+
+
+
+
+
+
